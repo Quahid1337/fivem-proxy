287a910c1993db4675e72fffd1c8ba5d15fc2abe
Quahid1337 <86263949+Quahid1337@users.noreply.github.com>
2025-06-04 18:44:29 +0000

    dadada

--- api/server.js
+++ api/server.js
@@ -1,25 +1,33 @@
-import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
-  const page = await browser.newPage();
-  await page.goto('https://servers-frontend.fivem.net/api/servers/single/ajyydz', {
-    waitUntil: 'networkidle0',
-    timeout: 60000
-  });
-
-  const body = await page.evaluate(() => document.body.innerText);
+  let browser = null;
 
   try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('https://servers-frontend.fivem.net/api/servers/single/ajyydz', {
+      waitUntil: 'networkidle0',
+      timeout: 60000
+    });
+
+    const body = await page.evaluate(() => document.body.innerText);
+
     const json = JSON.parse(body);
     res.status(200).json(json);
-  } catch (e) {
-    res.status(500).json({ error: "Failed to parse JSON", raw: body });
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
-
-  await browser.close();
 }
